928a92a22a54bb2d23be6c517642da44c0d698fb
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-08 22:23:20 +0300

    finished task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients');
+
+const ingredientRefs = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = document.createElement('li');
+    ingredient.textContent = ingredients[i];
+    ingredient.classList = 'item';
+    
+  ingredientRefs.push(ingredient);
+};
+
+list.append(...ingredientRefs);
+
