ce81137eeb9f7b8710e2a55ea1bfba128444fe2c
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-09 21:41:04 +0300

    finished task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const textBgc = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  changeColorBtn.style.backgroundColor = color;
+  textBgc.textContent = color;
+});
+
+
+
