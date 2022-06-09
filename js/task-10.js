b39a594f549541e6df87e58ce73915496cab1d60
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-09 22:51:49 +0300

    going task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('.boxes');
+
+const collectionBoxes = [];
+
+inputNumber.addEventListener('input', () => {
+  console.log(inputNumber.value);
+
+    createBtn.addEventListener('click', () => {
+      for (let i = 0; i < inputNumber.value; i += 1) {
+        const size = 30 + i * 10;
+        const box = document.createElement('div');
+        // box.classList.add(`box-${i}`);
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.style.backgrouhdColor = getRandomHexColor();
+        console.log(box);
+
+        collectionBoxes.push(box);
+      }
+    });
+});
+
+// boxes.append(...collectionBoxes);
+
+
+console.log(collectionBoxes);
