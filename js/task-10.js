2b75ccb33e40992bd83eb2fc1f0b7de8ddc98705
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-10 14:26:24 +0300

    finished task-10

--- js/task-10.js
+++ js/task-10.js
@@ -2,33 +2,46 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const inputNumber = document.querySelector('input');
+const inputData = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
-const boxes = document.querySelector('.boxes');
-
+const boxes = document.querySelector('#boxes');
 const collectionBoxes = [];
 
-inputNumber.addEventListener('input', () => {
-  console.log(inputNumber.value);
+const createBoxes = () => {
+  if (inputData.value <= 100) {
+    for (let i = 0; i < inputData.value; i += 1) {
+      const size = 30 + i * 10;
+      const box = document.createElement('div');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.marginTop = '1px';
+      box.style.fontSize = `${i + 8}px`;
+      box.textContent = `box ${i + 1}`;
+      box.style.textAlign = 'center';
+      box.style.backgroundColor = getRandomHexColor();
+      collectionBoxes.push(box);
+    }
+    boxes.append(...collectionBoxes);
+  } else {
+    const box = document.createElement('div');
+    box.style.color = 'tomato';
+    box.textContent = 'WARNING! the number should not be more than 100!';
+    boxes.append(box);
+  };
+};
 
-    createBtn.addEventListener('click', () => {
-      for (let i = 0; i < inputNumber.value; i += 1) {
-        const size = 30 + i * 10;
-        const box = document.createElement('div');
-        // box.classList.add(`box-${i}`);
-        box.style.width = `${size}px`;
-        box.style.height = `${size}px`;
-        box.style.backgrouhdColor = getRandomHexColor();
-        // console.log(box);
+const destroyBoxes = () => {
+  inputData.value = '';
+  collectionBoxes.splice(0, collectionBoxes.length);
+  boxes.innerHTML = '';
+};
 
-        collectionBoxes.push(box);
-      }
-      console.log(collectionBoxes);
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-    });
-});
+     
 
-// boxes.append(...collectionBoxes);
+
 
 
