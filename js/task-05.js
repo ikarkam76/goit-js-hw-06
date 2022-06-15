2b7c22e08ea7bc982ca0c7b2ad54655fa4bd63db
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-15 13:20:31 +0300

    correct task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,9 @@
 const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
 
-inputRef.addEventListener('input', (event) => {
-    if (!event.currentTarget.value) {
-      inputRef.removeEventListener('input');
-    }
-    outputRef.textContent = event.currentTarget.value;
-    
-});
+inputRef.addEventListener('input', onInputName);
+
+function onInputName(evt) {
+    !evt.currentTarget.value ?
+        outputRef.textContent = 'Anonymous':outputRef.textContent = evt.currentTarget.value;
+}
